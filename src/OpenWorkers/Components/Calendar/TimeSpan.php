cfc248b24395096904d47b7cceb0a4f7b1f06816
Frank Hildebrandt <frank.hildebrandt@me.com>
2014-03-19 22:35:27 +0100

    Implemented getDaysInMonth function

--- src/OpenWorkers/Components/Calendar/TimeSpan.php
+++ src/OpenWorkers/Components/Calendar/TimeSpan.php
@@ -3,23 +3,51 @@
 namespace OpenWorkers\Components\Calendar;
 
 
-use DateTime;
-
 class TimeSpan
 {
     /**
-     * @var DateTime
+     * @var SimpleDateTime
      */
     private $start;
     /**
-     * @var DateTime
+     * @var SimpleDateTime
      */
     private $end;
 
-    public function __construct(DateTime $start, DateTime $end)
+    public function __construct(SimpleDateTime $start, SimpleDateTime $end)
     {
         $this->start = $start;
         $this->end = $end;
+    }
+
+    /**
+     * Returns the matching days for a given month
+     * @param $month
+     * @return int
+     */
+    public function getDaysInMonth($month)
+    {
+        if ($this->start->getYear() < $this->end->getYear()) {
+            return cal_days_in_month(CAL_GREGORIAN, $month, $this->start->getYear());
+        }
+        $localStart = null;
+        $localEnd = null;
+        if ($this->start->getMonth() == $month) {
+            $localStart = $this->start;
+        }
+        if ($this->end->getMonth() == $month) {
+            $localEnd = $this->end;
+        }
+
+        if ($localStart == null) {
+            $localStart = SimpleDateTime::createFromFormat("Y-m-d", $this->start->getYear() . "-$month-01");
+        }
+        if ($localEnd == null) {
+            $localEnd = SimpleDateTime::createFromFormat("Y-m-d", $this->start->getYear() . "-" . ($month + 1) . "-01");
+        }
+
+        $diff = new TimeSpan($localStart, $localEnd);
+        return $diff->getDaysBetween();
     }
 
     /**
@@ -29,11 +57,5 @@ class TimeSpan
     public function getDaysBetween()
     {
         return $this->start->diff($this->end)->d;
-    }
-
-    public function getDaysInMonth($month)
-    {
-        return $this->start->diff($this->end)->d;
-
     }
 }
--- tests/OpenWorkers/ComponentsTests/Calendar/TimeSpanTest.php
+++ tests/OpenWorkers/ComponentsTests/Calendar/TimeSpanTest.php
@@ -3,6 +3,7 @@
 namespace OpenWorkers\ComponentsTests\Calendar;
 
 
+use OpenWorkers\Components\Calendar\SimpleDateTime;
 use OpenWorkers\Components\Calendar\TimeSpan;
 use PHPUnit_Framework_TestCase;
 
@@ -11,8 +12,8 @@ class TimeSpanTest extends PHPUnit_Framework_TestCase
 
     public function testDaysBetween()
     {
-        $start = \DateTime::createFromFormat("Y-m-d", "2000-01-01");
-        $end = \DateTime::createFromFormat("Y-m-d", "2000-01-10");
+        $start = SimpleDateTime::createFromFormat("Y-m-d", "2000-01-01");
+        $end = SimpleDateTime::createFromFormat("Y-m-d", "2000-01-10");
 
         $timeSpan = new TimeSpan($start, $end);
         $this->assertEquals(9, $timeSpan->getDaysBetween());
@@ -20,10 +21,11 @@ class TimeSpanTest extends PHPUnit_Framework_TestCase
 
     public function testDaysinMonth()
     {
-        $start = \DateTime::createFromFormat("Y-m-d", "2000-01-01");
-        $end = \DateTime::createFromFormat("Y-m-d", "2000-01-10");
+        $start = SimpleDateTime::createFromFormat("Y-m-d", "2000-01-30");
+        $end = SimpleDateTime::createFromFormat("Y-m-d", "2000-02-05");
 
         $timeSpan = new TimeSpan($start, $end);
-        $this->assertEquals(9, $timeSpan->getDaysInMonth(1));
+        $this->assertEquals(2, $timeSpan->getDaysInMonth(1));
+        $this->assertEquals(4, $timeSpan->getDaysInMonth(2));
     }
 }
